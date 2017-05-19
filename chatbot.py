d923249356de52d6c6c6739a2026cfb6a33a2ca7
Daniel Hardy <danisthebest8@users.noreply.github.com>
2017-05-19 18:07:43 +0100

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,25 @@
+#attempt 1 at chat bot
+
+print(r'''Hey! I'm Chris, What is your name?''')
+
+person = input('Enter Your Name: ')
+
+print('Hi!', person)
+
+print('How are you?')
+
+dc = input('How are you feeling:', )
+
+print('I feel Blue da ba dee')
+
+print('What is your favourite movie?')
+
+movie = input('Your favourite movie:', )
+
+print(movie ,'that is cool but I love the matrix')
+
+print('Lets do math! give me two numbers to multiply')
+
+num1 = input('First number:', )
+num2 = input('Second number:', )
+print int("num1"*"num2")
